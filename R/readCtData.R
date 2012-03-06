354024273dcfb70ad89a16b64a3416a3c6cb9a61
Heidi Dvinge <heidi@ebi.ac.uk>
2012-03-06 22:06:14 +0000

    Removed typo in readCtData
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/HTqPCR@63555 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/readCtData.R
+++ R/readCtData.R
@@ -108,7 +108,7 @@ function(files,
                	"Name of the qPCR feature (gene)",
                	"Type pf feature",
                	"Position on assay"))
-			featData	<- AnnotatedDataFrame(data=df, varMetaData=metaData)
+			featData	<- AnnotatedDataFrame(data=df, varMetadata=metaData)
 		}
 	}
 	# Provide sample names
@@ -134,9 +134,8 @@ function(files,
 	# Add some sort of phenoData
 	if (missing(sample.info)) {
 		pdata <- data.frame(sample=1:length(samples), row.names=samples)
-#		sample.info <- new("AnnotatedDataFrame", data = pdata,
-#           varMetadata = data.frame(labelDescription = "Sample numbering", row.names = "Sample names"))
-		sample.info <- new("AnnotatedDataFrame", data = pdata)
+		sample.info <- new("AnnotatedDataFrame", data = pdata,
+           varMetadata = data.frame(labelDescription = "Sample numbering", row.names = "Sample names"))
 	}
 	# Define the 'history'
 	X.hist	<- data.frame(history=capture.output(match.call(readCtData)), stringsAsFactors=FALSE)
